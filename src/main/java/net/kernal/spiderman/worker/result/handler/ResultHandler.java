46bde7e5ba88da5e5005a0afca52d04602d09505
qwop <qwop@live.cn>
2017-05-16 22:56:56 +0800

    添加对象模型文件 qwop.spiderman2.oom 结果处理接口注释

--- src/main/java/net/kernal/spiderman/worker/result/handler/ResultHandler.java
+++ src/main/java/net/kernal/spiderman/worker/result/handler/ResultHandler.java
@@ -6,11 +6,29 @@ import java.util.concurrent.atomic.AtomicReference;
 import net.kernal.spiderman.kit.Context;
 import net.kernal.spiderman.kit.Counter;
 import net.kernal.spiderman.worker.result.ResultTask;
-
+/**
+ * 结果处理接口
+ * JDK8新特性默认方法， init ，实现类将会默认继承该方法,也可以覆盖这个方法。
+ * <结果处理> 
+ * <通过原子引用 Context 来保证 context 上下文是线程安全的> 
+ * 
+ * @author		qwop
+ * @date 		May 16, 2017 
+ * @version		[The version number, May 16, 2017] 
+ * @see			[Related classes/methods] 
+ * @since		[Products/Module version]
+ */
 public interface ResultHandler {
 	
 	final AtomicReference<Context> context = new AtomicReference<Context>();
-
+	
+	/**
+	 * 默认实现加载上下文、获取配置参数 worker.result.store 来保存数据
+	 * @param ctx 
+	 * @author	qwop
+	 * @date	May 16, 2017
+	 * @see [Class、Class#Method、Class#Field]
+	 */
 	public default void init(Context ctx) {
 		context.set(ctx);
 		final String savePath = ctx.getParams().getString("worker.result.store", "store/result");
--- src/main/java/net/kernal/spiderman/worker/result/handler/impl/ConsoleResultHandler.java
+++ src/main/java/net/kernal/spiderman/worker/result/handler/impl/ConsoleResultHandler.java
@@ -7,6 +7,17 @@ import net.kernal.spiderman.worker.extract.ExtractResult;
 import net.kernal.spiderman.worker.result.ResultTask;
 import net.kernal.spiderman.worker.result.handler.ResultHandler;
 
+/**
+ * 
+ * <控制台结果处理类> 
+ * <结果处理> 
+ * 
+ * @author		qwop
+ * @date 		May 16, 2017 
+ * @version		[The version number, May 16, 2017] 
+ * @see			[Related classes/methods] 
+ * @since		[Products/Module version]
+ */
 public class ConsoleResultHandler implements ResultHandler {
 	public void handle(ResultTask task, Counter c) {
 		final ExtractResult er = task.getResult();
